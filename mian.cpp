691533a4dbb7469d61c5f9e70eafd08606b005bf
platelets <wenkanedward@gmail.com>
2022-11-20 10:52:58 +0800

    init

---
+++ mian.cpp
@@ -0,0 +1,14 @@
+/*
+ * @LastEditors: platelets wenkanedward@gmail.com
+ * @Date: 2022-11-20 01:06:53
+ * @LastEditTime: 2022-11-20 01:45:09
+ * Copyright (c) 2022 by platelets email: wenkanedward@gmail.com, All Rights Reserved.
+ */
+#include <iostream>
+
+#include "adder.h"
+int main(){
+    std::cout<<"Hello cmake"<< std::endl; 
+    std::cout<<add(123.4,245.2) << "\n";
+    return 0;
+}
